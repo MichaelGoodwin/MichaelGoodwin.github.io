ba60d62963757adaa48753be4504da5eb12ad0bf
TheStonedTurtle <mgoodwin559@gmail.com>
2019-03-22 16:43:13 -0700

    Update Mello Trainer project view

--- Portfolio/src/app/project-details/details/mello-trainer/mello-trainer.component.ts
+++ Portfolio/src/app/project-details/details/mello-trainer/mello-trainer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Lightbox, IAlbum, LightboxConfig } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-mello-trainer',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mello-trainer.component.css']
 })
 export class MelloTrainerComponent implements OnInit {
+  public images: string[] = [
+    '/assets/imgs/MelloTrainer/card.png',
+    '/assets/imgs/MelloTrainer/vehicle-options.png',
+    '/assets/imgs/MelloTrainer/weapons.png',
+    '/assets/imgs/MelloTrainer/generic.png',
+    '/assets/imgs/MelloTrainer/version-check.png'
+  ];
+  private album: Array<IAlbum> = [];
 
-  constructor() { }
+  constructor(
+    private lightbox: Lightbox,
+    private lightboxConfig: LightboxConfig) {
+    for (const img of this.images) {
+      this.album.push({src: img, thumb: img});
+    }
+
+    // set default config
+    this.lightboxConfig.fadeDuration = 1;
+   }
 
   ngOnInit() {
   }
 
+  open(index: number): void {
+    console.log(this.images[index]);
+    this.lightbox.open(this.album, index);
+  }
+
+  close(): void {
+    this.lightbox.close();
+  }
 }
