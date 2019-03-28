8ee244df73d57adfd45337942610a92b80ecf1d8
TheStonedTurtle <mgoodwin559@gmail.com>
2019-03-28 03:12:31 -0700

    Fix project order

--- Portfolio/src/app/services/project/Projects.ts
+++ Portfolio/src/app/services/project/Projects.ts
@@ -10,14 +10,15 @@ import { KeptItemsComponent } from 'src/app/project-details/details/kept-items/k
 const imageFolder = '/assets/imgs/';
 
 export default [
-  new Project(MelloTrainerComponent, 'Mello Trainer', ProjectCategory.FiveM, imageFolder + 'MelloTrainer/card.png', ['lua', 'html', 'css'],
-    'A FiveM tool used to spawn vehicle/weapons, control the time & weather, and much more customization'),
-  new Project(RPDeathComponent, 'RPDeath', ProjectCategory.FiveM, imageFolder + 'RPDeath/card.png', ['lua'],
-    'A FiveM script that prevents automatic respawning for role-playing purposes'),
   new Project(SkillCalculatorComponent, 'SkillCalculator', ProjectCategory.RuneLite, imageFolder + 'SkillCalculator/card.png', ['java'],
     'Expands upon the existing "SkillCalculator" plugin by adding Planner and Banked Experience functionality'),
   new Project(LootTrackerComponent, 'Loot Tracker', ProjectCategory.RuneLite, imageFolder + 'LootTracker/card.png', ['java'],
     'Records loot received from monsters, players, and activities and displays it in a side panel'),
   new Project(KeptItemsComponent, 'Kept Items', ProjectCategory.RuneLite, imageFolder + 'KeptItems/card.png', ['java'],
     'Reworks the existing Items Kept on Death interface so that it is more accurate and interactive'),
+
+  new Project(MelloTrainerComponent, 'Mello Trainer', ProjectCategory.FiveM, imageFolder + 'MelloTrainer/card.png', ['lua', 'html', 'css'],
+    'A FiveM tool used to spawn vehicle/weapons, control the time & weather, and much more customization'),
+  new Project(RPDeathComponent, 'RPDeath', ProjectCategory.FiveM, imageFolder + 'RPDeath/card.png', ['lua'],
+    'A FiveM script that prevents automatic respawning for role-playing purposes'),
 ];
