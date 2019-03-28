05805b9d651f8320b7250f57f300f6cfa97063f8
TheStonedTurtle <mgoodwin559@gmail.com>
2019-03-28 02:45:25 -0700

    Fix lint error

--- Portfolio/src/app/navbar/navbar.component.ts
+++ Portfolio/src/app/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Subscription } from 'rxjs';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit,OnDestroy {
+export class NavbarComponent implements OnInit, OnDestroy {
   navbarCollapsed = true;
   private sub: Subscription;
 
