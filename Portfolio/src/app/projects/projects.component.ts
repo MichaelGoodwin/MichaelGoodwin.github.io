83b083a5c3571715e9cf8b1c183381b869d03b28
MichaelGoodwin <7521194+MichaelGoodwin@users.noreply.github.com>
2019-08-03 04:35:24 -0700

    Add MarsRovers project

--- Portfolio/src/app/projects/projects.component.ts
+++ Portfolio/src/app/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../services/project/project.service';
 import { ProjectCategory } from '../services/project/ProjectCategory';
+import { Project } from '../services/project/Project';
 
 @Component({
   selector: 'app-projects',
@@ -9,6 +10,10 @@ import { ProjectCategory } from '../services/project/ProjectCategory';
 })
 export class ProjectsComponent implements OnInit {
   categories: Array<ProjectCategory>;
+  websiteProjects: Array<Project> = [
+    new Project(null, 'Mars Rovers', null, './assets/imgs/MarsRovers/card.png', ['Angular', 'JavaScript', 'TypeScript', 'HTML', 'CSS'],
+    'A website to explore the images the rovers exploring mars have taken')
+  ];
 
   constructor(private projectService: ProjectService) {
     this.categories = projectService.getCategories();
